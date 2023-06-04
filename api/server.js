e647eda9813598801dcd1109a1b5136d13ae5fe2
tharinm <tharindumadhushan864@gmail.com>
2023-06-04 12:15:15 +0530

    change cors origin

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ dotenv.config()
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://weather-f.onrender.com/", credentials: true }));
 
 const connect = async () => {
   try {
