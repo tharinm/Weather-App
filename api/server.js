68014dfbb68d1e4e919d4b79750b361814aca9d3
tharinm <tharindumadhushan864@gmail.com>
2023-06-04 21:48:04 +0530

    fix issues

--- api/server.js
+++ api/server.js
@@ -2,18 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import userRoutes from './routes/user.routes.js'
+import userRoutes from "./routes/user.routes.js";
 import cors from "cors";
 
-
 const app = express();
-dotenv.config()
+dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 
 //localhost:3000
-http: app.use(cors({ origin: "https://weather-f.onrender.com", credentials: true }));
+http: app.use(
+  cors({ origin: "https://weather-f.onrender.com", credentials: true })
+);
 
 const connect = async () => {
   try {
@@ -24,10 +25,9 @@ const connect = async () => {
   }
 };
 
-
 app.use("/api/auth", userRoutes);
 
 app.listen(8000, () => {
-    connect();
+  connect();
   console.log("Server is running !");
 });
